8641f36de8a28bea0c253a2cf73161a6b9312e2a
FrancescVargas <fravase76@gmail.com>
2016-09-13 11:30:50 +0200

    modificado index.php

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 require "vendor/autoload.php";
 $app=new Slim\App();
-$c=$app->getContainer();
 $app->get("/",function($request,$response,$args)
           {
               $response->write("<h1>Entrenadores</h1>");
