442a8ec0e6a9bb6f87f1dbb486b18f0b61be4467
FrancescVargas <fravase76@gmail.com>
2016-09-13 14:23:54 +0200

    primera vista (php-view)

--- index.php
+++ index.php
@@ -1,10 +1,22 @@
 <?php
 require "vendor/autoload.php";
 $app=new Slim\App();
+$c=$app->getContainer();
+
+$c["bd"]=function()
+{
+    $pdo=new PDO("mysql:host=localhost;dbname=entrenador","root");
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
+    return $pdo;
+};
+
+
+$c["view"]=new \Slim\Views\PhpRenderer("vista/");
+
 $app->get("/",function($request,$response,$args)
           {
-              $response->write("<h1>Entrenadores</h1>");
-              return $response;
+            $response=$this->view->render($response,"plantillaprimera.php",[]);
+            return $response;
           });
 $app->run();
 ?>
---
+++ vista/plantillaprimera.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="es">
+<head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width; initial-scale=1.0">
+        <title>Entrenador-Inicio</title>
+
+
+        <style>
+
+        </style>
+</head>
+
+<body>
+<h1>Entrenador</h1>
+<h3>-Inicio-</h3>
+</body>
+
+
+
+
+</html>
